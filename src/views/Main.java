db69a04fea60859c4e12586be4af7d21f4022ba0
Edwin Ropain E. <edwinropain260@outlook.com>
2024-12-18 16:16:54 -0500

    Primer commit

---
+++ src/views/Main.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package views;
+
+/**
+ *
+ * @author pc
+ */
+public class Main {
+    public static void main(String[] args) {
+        Ventana_principal miVentana_principal2 = new Ventana_principal();
+        miVentana_principal2.setVisible(true);
+    }
+}
